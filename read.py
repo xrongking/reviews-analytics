04bca3c61109ea20235701bac57250450253238d
Sanya Yang <xrongking@gmail.com>
2020-05-28 16:54:38 +0800

    add new function

--- read.py
+++ read.py
@@ -13,3 +13,9 @@ sum_len = 0
 for d in data:
 	sum_len += len(d)
 print('留言平均長度為', sum_len / len(data))
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '筆留言長度小於100')
